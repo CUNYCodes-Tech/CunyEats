2485b99d55bd2898491d390c685ea4e3ac30b13c
rojan <rojankc08@gmail.com>
2019-09-19 19:27:06 -0400

    First frontend Pushed

---
+++ app/src/main/java/com/example/cunyeats/signUpActivity.java
@@ -0,0 +1,22 @@
+package com.example.cunyeats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class signUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+//    Spinner spinner = (Spinner) findViewById(R.id.selectCampus);
+//
+//    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.campusArray,
+//                                                                        android.R.layout.simple_spinner_dropdown_item);
+//    spinner.setAdapter(adapter);
+}
